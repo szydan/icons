f952e6c03b8d370410f41dd51ac2c13888d217a3
Szymon Danielczyk <danielczyk.szymon@gmail.com>
2015-12-15 22:54:20 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+Custome icons
+=============
+
+Made thanks to awesome project [fontcustom](https://github.com/FontCustom/fontcustom) 
+
+To regenerate:    
+ 
+* add your vector *.svg file to **vectors** folder
+* follow the instructions at [fontcustom](https://github.com/FontCustom/)
+
+
---
+++ bower.js
@@ -0,0 +1,23 @@
+{
+  "name": "icons",
+  "description": "Icons",
+  "keywords": [],
+  "dependencies": {},
+  "devDependencies": {},
+  "license": [
+    "MIT"
+  ],
+  "main": [
+    "fontcustom/fontcustom.css"
+  ],
+  "ignore": [
+    "*/.*",
+    "*.json",
+    "src",
+    "*.yml",
+    "Gemfile",
+    "Gemfile.lock",
+    "*.md"
+  ],
+  "version": "1.0.0"
+}
